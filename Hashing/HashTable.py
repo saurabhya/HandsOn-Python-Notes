38b6302496bed4d6306fcb327352b208b1f7bac2
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-03-21 19:33:07 +0530

    adding code

--- Hashing/HashTable.py
+++ Hashing/HashTable.py
@@ -1,19 +1,19 @@
 '''
-A hash table is data structure where elements are accessed by keyword rather 
-than an index number, unlike in lists and arrays. In this data structure, the data 
+A hash table is data structure where elements are accessed by keyword rather
+than an index number, unlike in lists and arrays. In this data structure, the data
 items are stored as key/value pairs similar to dictionaries.
 A hash table uses a hashing function in prder to find an index position
 where an element should be stored and retrieved. This gives us fast
-lookups since we are using an index number that corresponds to the hash 
+lookups since we are using an index number that corresponds to the hash
 value of the key.
 
-Each position in the hash table is often called a slot or bucket and can 
+Each position in the hash table is often called a slot or bucket and can
 store an element. So, each data item in the form of (key, value) pairs would
-be stored in the hash table at a position that is decided by the hash value 
+be stored in the hash table at a position that is decided by the hash value
 of the data.
 
-To implement the hash table, we start by creating a class to hold hash table 
-items. These need to have a key and a value since hash table is 
+To implement the hash table, we start by creating a class to hold hash table
+items. These need to have a key and a value since hash table is
 a {key, value} store.
 '''
 
@@ -26,7 +26,7 @@ class HashItem:
         return '{'+str(self.key)+' : '+str(self.value)+'}'
 
 '''
-This gives us a very simple way to store items. Next we strat working 
+This gives us a very simple way to store items. Next we strat working
 on the hash table class itself.
 
 '''
@@ -36,4 +36,27 @@ class HashTable:
         self.size = 256
         self.slots = [None for i in range(self.size)]
         self.count = 0
+
+    '''
+    The hash table uses a standard Python list to store its elements.
+    Let's set the size of the hash table to 256 elements to start with.
+    Later we will look at the strategies for how to grow thw hash table as we begin
+    filling it up.
+    '''
+    def _hash(self, key):
+        mult = 1
+        hv = 0
+        for ch in key:
+            hv += mult * ord(ch)
+            mult += 1
+        return hv%self.size
+    '''
+    To store the elements in the hash table ,we will be using put() function.
+    and retrieve them using get() function.
+    '''
+
+    def put(self, key, value):
+        item = HashItem(key, value)
+        h = self._hash(key)
         
+
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Handson data structures and algorithms in Python
-Recently I came around this book and started reading it, this book is helpful for people who are intersted in implementing data structures
- and algorithms in Python.
+
+Recently I came around this book and started reading it, this book is helpful for people who are intersted in implementing data structures and algorithms in Python by Dr. Basant agarwal and Benjamin Baka.
  </br>
- Due to the corona virus outbreak I had to extend to my holidays so I have just tried to implement the codes given in the book. Codes have 
- not been written with the focus towards a reader. I have tried to include as much comments as possible but there might be some places where 
+ Due to the corona virus outbreak I had to extend to my holidays so I have just tried to implement the codes given in the book. Codes have
+ not been written with the focus towards a reader. I have tried to include as much comments as possible but there might be some places where
  you need to refer the book.
  </br>
  Book is very informative, so I would like to suggest this book to anyone who is looking to extend his knowledge of Python.
