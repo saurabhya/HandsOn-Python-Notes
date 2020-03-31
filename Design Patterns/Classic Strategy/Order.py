d13fb655287ee8070e53562c4b362d415434a5a3
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-03-31 23:11:21 +0530

    testing classic strategy

--- Design Patterns/Classic Strategy/Order.py
+++ Design Patterns/Classic Strategy/Order.py
@@ -40,7 +40,7 @@ class Order:    # the context
         return self.__total
 
     def due(self):
-        if slef.promotion is None:
+        if self.promotion is None:
             discount = 0
         else:
             discount = self.promotion.discount(self)
@@ -83,8 +83,25 @@ class LargeOrderPromo(Promotion):
     """
 
     def discount(self, order):
-        distinct_items = {item.product got item in order.cart}
+        distinct_items = {item.product for item in order.cart}
         if len(distinct_items) >= 10:
             return order.total()*.07
 
         return 0
+
+
+joe = Customer('John Doe', 0)
+ann = Customer('Ann Smith', 1100)
+cart = [LineItem('banana', 4, .5), LineItem('apple', 10, 1.5), LineItem('watermelon', 5, 5.0)]
+
+print(Order(joe, cart, FidelityPromo()))
+print(Order(ann, cart, FidelityPromo()))
+
+banana_cart = [LineItem('banana', 30, .5), LineItem('apple', 10, 1.5)]
+
+print(Order(joe, banana_cart, BulkItemPromo()))
+
+long_order = [LineItem(str(item_code), 1, 1.0) for item_code in range(10)]
+
+print(Order(joe, long_order, LargeOrderPromo()))
+print(Order(joe, cart, LargeOrderPromo()))
