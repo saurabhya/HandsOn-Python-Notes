453a13f0727612fa13ad74eda3d99752f798fb2a
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-04-06 13:17:49 +0530

    some update

--- Object-oriented idioms/mutables_parameter_defaults.py
+++ Object-oriented idioms/mutables_parameter_defaults.py
@@ -45,9 +45,23 @@ print(bus2.passengers is bus3.passengers)
 print(bus1.passengers)
 
 """
-    The problem is that Bus instances don't get an initialpassenger list end up sharinng the same passenger list among
+    The problem is that Bus instances don't get an initial passenger list end up sharing the same passenger list among
     themselves.
     Such bugs may be subtle. When HauntedBus is instantiated with passengers, it works as expected. Strange things
     happen only a HauntedBus start empty, because then self.passengers becomes an alias for the default value of the
-    passengers parameter.
+    passengers parameter. The problem is that each default value is evaluated when the function is defined - i.e.,
+    usually when the module is loaded and the default value become attributes of the function object.
+    So if a default value is mutable object, and you change it, the change will affect every future call of the function.
+    You can inspect the HauntedBus.__init object  and see the ghost students haunting its __default__ attributes:
+"""
+print(dir(HauntedBus.__init__))
+print(HauntedBus.__init__.__defaults__)
+
+# finally we can verify that bus2.passengers is an alias bound to the first element of the HauntedBus.__init__.__defaults__
+# attribute:
+print(HauntedBus.__init__.__defaults__[0] is bus2.passengers)
+
+"""
+    The issue with mutable defaults explains why None is often used used as the default value for parameters
+    that may recieve mutable values.
 """
