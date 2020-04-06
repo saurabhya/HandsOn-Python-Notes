233f7f9d1e324a685d45c8b3221cd2176af09f00
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-04-06 23:37:21 +0530

    some changes

--- SingleLinkedList.py
+++ SingleLinkedList.py
@@ -18,7 +18,7 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return str(data)
+        return str(self.data)
 
 
 class SinglyLinkedList:
@@ -26,26 +26,26 @@ class SinglyLinkedList:
         self.tail = None
         self.head = None
     # The first operation that we need to perform is to append items to the list.
-    # This operation is somtimes called insert operation. Here we get a chance 
-    # to hide away the node class. The user of our list class should really 
+    # This operation is somtimes called insert operation. Here we get a chance
+    # to hide away the node class. The user of our list class should really
     # never have to interact with Node objects. These are purely for internal use.
     def append(self, data):
         '''
-        There is a big problem with append method i.e. maintaining only a single 
-        pointer for the start of the list may cause running time of the insertion 
+        There is a big problem with append method i.e. maintaining only a single
+        pointer for the start of the list may cause running time of the insertion
         to be O(n) where n is the length of the list. For lists where n is very large,
-        It presents a huge issue. 
+        It presents a huge issue.
 
         What can be the solution for that ?
 
         We can maintain an additional pointer for the end of the list that will help
-        to reduce the running time of insertion from O(n) to O(1).  
+        to reduce the running time of insertion from O(n) to O(1).
         '''
         # Encapsulate data in a Node
         node = Node(data)
         if self.head:
             self.head.next = node
-            self.head = node 
+            self.head = node
         else:
             self.tail = node
             self.head = node
@@ -53,18 +53,18 @@ class SinglyLinkedList:
     '''
     def __repr__(self):
         res = []
-        current = self.tail 
+        current = self.tail
         while current:
             res.append(current.data)
             res.append('->')
             current = current.next
-        
+
         return "".join(res)
 
     Above function uses magic function __repr__ to showcase list traversal.
     But if you notice in the above list traversal, where we are exposing the node class to
-    the client/user. however, it is desirable that the client node should not interact with 
-    the node object. We need to use node.data to get the contents of the node and node.next 
+    the client/user. however, it is desirable that the client node should not interact with
+    the node object. We need to use node.data to get the contents of the node and node.next
     to get the next node. We can access the data by creating a method that returns a generator.
     '''
     def iter(self):
@@ -75,9 +75,9 @@ class SinglyLinkedList:
             yield val
     '''
     Next operation we have to consider in the list is deletion.
-    When we want to delete a node that is between two other nodes, all we have to do is we 
+    When we want to delete a node that is between two other nodes, all we have to do is we
     make the previous node point to the successor of its next node that is to be deleted.
-    Following is the proposed solution.  
+    Following is the proposed solution.
     '''
     def delete(self, data):
         current = self.tail
@@ -88,36 +88,35 @@ class SinglyLinkedList:
                     self.tail = current.next
                 else:
                     prev.next = current.next
-                self.count -= 1
                 return
             prev = current
             current = current.next
 
     '''
-    We may also need a way to check whether a list conains an item. This method is 
-    fairly easy to implement thanks to iter() method we previously wrote. Each pass 
-    of the loop compares the current data to the data being searched. If a match is 
+    We may also need a way to check whether a list conains an item. This method is
+    fairly easy to implement thanks to iter() method we previously wrote. Each pass
+    of the loop compares the current data to the data being searched. If a match is
     found, True is returned, or else False is returned:
     '''
     def search(self, data):
         for node in self.iter():
             if data == node :
                 return True
-            
+
         return False
 
-    
+
     '''
     Clearing a list?
 
-    We may need to clear a list quickly; there is a very simple way to do it. 
-    We can clear a list by simply clearing the pointer head and tail by 
+    We may need to clear a list quickly; there is a very simple way to do it.
+    We can clear a list by simply clearing the pointer head and tail by
     setting them to None.
 
     '''
     def clear(self):
         self.tail = None
-        self.head = NoneS
+        self.head = None
 
 
 words = SinglyLinkedList()
