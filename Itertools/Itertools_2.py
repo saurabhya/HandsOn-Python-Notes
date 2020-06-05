526519f69d85dc0273cf22fb7a34c3dab5f74f98
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-05 22:59:30 +0530

    adding zip_longest()

--- Itertools/Itertools_2.py
+++ Itertools/Itertools_2.py
@@ -15,4 +15,15 @@ print(list(better_grouper(nums, 4)))
     The elements 9 and 10 are missing from the grouped output. This happens because zip() stops
     aggregating elements once the shortest iterable passed to it is exhausted. It would make more
     sense to return a third group containing 9 and 10.
+
+    To do this, you can itertools.zip_longest(). This function accepts any number of iterables as arguments
+    and a fillvalue keyword argument that defaults to None. The easiest way to get a sense of the difference
+    between zip() and zip_longest() is to look at some example output:
 """
+
+import itertools as it
+
+x = [1,2,3,4,5]
+y = ['a', 'b', 'c']
+print(list(zip(x, y)))
+print(list(it.zip_longest(x,y)))
