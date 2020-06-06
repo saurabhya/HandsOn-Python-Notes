e4d227e804c4ed6c8151412669b22518eebea98e
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-06 17:21:55 +0530

    adding itertools.repeat()

--- Itertools/Itertools_4.py
+++ Itertools/Itertools_4.py
@@ -24,3 +24,22 @@ def fibs():
 
 count_by_three = it.count(step=3)
 count_by_four = it.count(step= 4)
+
+"""
+    In fact, count() can produce sequences of multiple of any number you wish. These sequences can be
+    describedwith first-order recurrence relations.
+
+    Another easy example of a first-order recurrence relation is the constant sequence n, n, n, n..., where
+    n is any value you'd like. Itertools provides an easy way to implement this sequence as well, with the
+    repeat() function.
+"""
+
+all_ones = it.repeat(1)
+all_twos = it.repeat(2)
+
+"""
+    If you need a finite sequence of repeated values, you can set a stopping point by passing a positive
+    integer as a second argument.
+"""
+five_ones = it.repeat(1, 5)
+five_twos = it.repeat(4, 3)
