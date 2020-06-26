75c23673b26a4f5b6fa4f7fb163fbddeea4cbfd0
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-26 22:43:37 +0530

    debugging file

--- ordered_dict.py
+++ ordered_dict.py
@@ -1,8 +1,8 @@
 '''
-The important thing about ordered dictionaries is that they remember the insertion order, 
-so when we iterate over them, they return in the order they were inserted.
-for ordinary dictionaries equality is based on the key, value pairs
-while for ordered dictionaries order of insertion is also taken into consideration.
+    The important thing about ordered dictionaries is that they remember the insertion order,
+    so when we iterate over them, they return in the order they were inserted.
+    for ordinary dictionaries equality is based on the key, value pairs
+    while for ordered dictionaries order of insertion is also taken into consideration.
 '''
 import collections
 od1 = collections.OrderedDict()
@@ -14,8 +14,8 @@ od2['one'] = 1
 print(od1==od2)
 
 '''
-Similarly, when we add values from a list using update,
-OrderedDict will retain the same order as the list.
+    Similarly, when we add values from a list using update,
+    OrderedDict will retain the same order as the list.
 '''
 kvs = [('three',3),('four',4),('five',5)]
 od1.update(kvs)
@@ -24,6 +24,7 @@ for k,v in od1.items():
     print(k,v)
 
 ##############################################################
+# print(sorted(od1.items(), key= lambda t: (4*t[1]) - t[1]**2))
 od3 = collections.OrderedDict(sorted(od1.items(), key= lambda t: (4*t[1]) - t[1]**2))
 print(od3)
 print(od3.values())
