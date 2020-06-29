dac15fd700a0747835603cd0e85a748e10a93be8
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-29 22:33:32 +0530

    adding texts for the class

--- OOP_python.py
+++ OOP_python.py
@@ -1,3 +1,9 @@
+"""
+    Object-oriented programming in Python is very simple and easy to understand.
+    In the following example we will try declaring a class named Employee to explain how
+    OOP works in Pyhton.
+"""
+
 class Employee(object):
     numEmployee = 0
     def __init__(self,name, rate):
@@ -5,14 +11,14 @@ class Employee(object):
         self.name = 0
         self.rate = rate
         Employee.numEmployee += 1
-    
+
     def __del__(self):
         Employee.numEmployee -= 1
-    
+
     def hours(self,numHours):
         self.owed += numHours*self.rate
         return ("%.2f hours worked" %numHours)
-    
+
     def pay(self):
         self.owed = 0
         return ("payed %s"%self.name)
