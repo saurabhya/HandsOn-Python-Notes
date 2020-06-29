0b1d0ead58c953f8c9da25b71fbc8833e4e3d1a7
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-29 22:25:50 +0530

    adding comments

--- set.py
+++ set.py
@@ -1,5 +1,6 @@
 s1 = {'ab',3,4,(5,6)}
 s2 = {'ab',7,(7,6)}
-print(s1-s2)
+print(s1-s2) # sets allows '-' operation
+# Intersection as weel as union is also allowed for sets
 print(s1.intersection(s2))
 print(s1.union(s2))
