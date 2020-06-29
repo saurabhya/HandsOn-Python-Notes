6bb748f4d4763c11f6801439d1bde4750bc19c60
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-06-29 22:30:02 +0530

    formatting texts

--- Pointers.py
+++ Pointers.py
@@ -1,14 +1,13 @@
 '''
-In Python, you don't manipulate pointers directly, unlike in some other languages, such as C or Pascal.
-This has led some people to believe that pointers aren't used in Python.
-nothing is further from the truth.
+    In Python, you don't manipulate pointers directly, unlike in some other languages, such as C or Pascal.
+    This has led some people to believe that pointers aren't used in Python.
+    nothing is further from the truth.
 
-For example:
+    For example:
 
-s = set()
-here s is more a reference than a variable: set is an object created in the memory
-and a pointer(reference) is given to s. s points to the set object in the memory.
-
+    s = set()
+    here s is more a reference than a variable: set is an object created in the memory
+    and a pointer(reference) is given to s. s points to the set object in the memory.
 '''
 # Class Node
 
@@ -26,20 +25,20 @@ class SinglyLinkedList:
         self.tail = None
         self.head = None
     # The first operation that we need to perform is to append items to the list.
-    # This operation is somtimes called insert operation. Here we get a chance 
-    # to hide away the node class. The user of our list class should really 
+    # This operation is somtimes called insert operation. Here we get a chance
+    # to hide away the node class. The user of our list class should really
     # never have to interact with Node objects. These are purely for internal use.
     def append(self, data):
         '''
-        There is a big problem with append method i.e. maintaining only a single 
-        pointer for the start of the list may cause running time of the insertion 
-        to be O(n) where n is the length of the list. For lists where n is very large,
-        It presents a huge issue. 
+            There is a big problem with append method i.e. maintaining only a single
+            pointer for the start of the list may cause running time of the insertion
+            to be O(n) where n is the length of the list. For lists where n is very large,
+            It presents a huge issue.
 
-        What can be the solution for that ?
+            What can be the solution for that ?
 
-        We can maintain an additional pointer for the end of the list that will help
-        to reduce the running time of insertion from O(n) to O(1).  
+            We can maintain an additional pointer for the end of the list that will help
+            to reduce the running time of insertion from O(n) to O(1).
         '''
         # Encapsulate data in a Node
         node = Node(data)
@@ -53,19 +52,19 @@ class SinglyLinkedList:
     '''
     def __repr__(self):
         res = []
-        current = self.tail 
+        current = self.tail
         while current:
             res.append(current.data)
             res.append('->')
             current = current.next
-        
+
         return "".join(res)
 
-    Above function uses magic function __repr__ to showcase list traversal.
-    But if you notice in the above list traversal, where we are exposing the node class to
-    the client/user. however, it is desirable that the client node should not interact with 
-    the node object. We need to use node.data to get the contents of the node and node.next 
-    to get the next node. We can access the data by creating a method that returns a generator.
+        Above function uses magic function __repr__ to showcase list traversal.
+        But if you notice in the above list traversal, where we are exposing the node class to
+        the client/user. however, it is desirable that the client node should not interact with
+        the node object. We need to use node.data to get the contents of the node and node.next
+        to get the next node. We can access the data by creating a method that returns a generator.
     '''
     def iter(self):
         current = self.tail
@@ -74,10 +73,10 @@ class SinglyLinkedList:
             current = current.next
             yield val
     '''
-    Next operation we have to consider in the list is deletion.
-    When we want to delete a node that is between two other nodes, all we have to do is we 
-    make the previous node point to the successor of its next node that is to be deleted.
-    Following is the proposed solution.  
+        Next operation we have to consider in the list is deletion.
+        When we want to delete a node that is between two other nodes, all we have to do is we
+        make the previous node point to the successor of its next node that is to be deleted.
+        Following is the proposed solution.
     '''
     def delete(self, data):
         current = self.tail
@@ -94,26 +93,25 @@ class SinglyLinkedList:
             current = current.next
 
     '''
-    We may also need a way to check whether a list conains an item. This method is 
-    fairly easy to implement thanks to iter() method we previously wrote. Each pass 
-    of the loop compares the current data to the data being searched. If a match is 
-    found, True is returned, or else False is returned:
+        We may also need a way to check whether a list conains an item. This method is
+        fairly easy to implement thanks to iter() method we previously wrote. Each pass
+        of the loop compares the current data to the data being searched. If a match is
+        found, True is returned, or else False is returned:
     '''
     def search(self, data):
         for node in self.iter():
             if data == node :
                 return True
-            
+
         return False
 
-    
+
     '''
-    Clearing a list?
+        Clearing a list?
 
-    We may need to clear a list quickly; there is a very simple way to do it. 
-    We can clear a list by simply clearing the pointer head and tail by 
-    setting them to None.
-
+        We may need to clear a list quickly; there is a very simple way to do it.
+        We can clear a list by simply clearing the pointer head and tail by
+        setting them to None.
     '''
     def clear(self):
         self.tail = None
