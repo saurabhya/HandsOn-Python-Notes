0d28aeb43671ab419c4747ea484d9c08b89d2a9d
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-02 21:21:24 +0530

    updating comments

--- Hashing/HashTable.py
+++ Hashing/HashTable.py
@@ -1,20 +1,20 @@
 '''
-A hash table is data structure where elements are accessed by keyword rather
-than an index number, unlike in lists and arrays. In this data structure, the data
-items are stored as key/value pairs similar to dictionaries.
-A hash table uses a hashing function in prder to find an index position
-where an element should be stored and retrieved. This gives us fast
-lookups since we are using an index number that corresponds to the hash
-value of the key.
+    A hash table is data structure where elements are accessed by keyword rather
+    than an index number, unlike in lists and arrays. In this data structure, the data
+    items are stored as key/value pairs similar to dictionaries.
+    A hash table uses a hashing function in prder to find an index position
+    where an element should be stored and retrieved. This gives us fast
+    lookups since we are using an index number that corresponds to the hash
+    value of the key.
 
-Each position in the hash table is often called a slot or bucket and can
-store an element. So, each data item in the form of (key, value) pairs would
-be stored in the hash table at a position that is decided by the hash value
-of the data.
+    Each position in the hash table is often called a slot or bucket and can
+    store an element. So, each data item in the form of (key, value) pairs would
+    be stored in the hash table at a position that is decided by the hash value
+    of the data.
 
-To implement the hash table, we start by creating a class to hold hash table
-items. These need to have a key and a value since hash table is
-a {key, value} store.
+    To implement the hash table, we start by creating a class to hold hash table
+    items. These need to have a key and a value since hash table is
+    a {key, value} store.
 '''
 
 class HashItem:
@@ -26,9 +26,8 @@ class HashItem:
         return '{'+str(self.key)+' : '+str(self.value)+'}'
 
 '''
-This gives us a very simple way to store items. Next we strat working
-on the hash table class itself.
-
+    This gives us a very simple way to store items. Next we strat working
+    on the hash table class itself.
 '''
 
 class HashTable:
@@ -38,10 +37,10 @@ class HashTable:
         self.count = 0
 
     '''
-    The hash table uses a standard Python list to store its elements.
-    Let's set the size of the hash table to 256 elements to start with.
-    Later we will look at the strategies for how to grow thw hash table as we begin
-    filling it up.
+        The hash table uses a standard Python list to store its elements.
+        Let's set the size of the hash table to 256 elements to start with.
+        Later we will look at the strategies for how to grow thw hash table as we begin
+        filling it up.
     '''
     def _hash(self, key):
         mult = 1
@@ -51,25 +50,25 @@ class HashTable:
             mult += 1
         return hv%self.size
     '''
-    To store the elements in the hash table ,we will be using put() function.
-    and retrieve them using get() function.
+        To store the elements in the hash table ,we will be using put() function.
+        and retrieve them using get() function.
     '''
 
     def put(self, key, value):
         item = HashItem(key, value)
         h = self._hash(key)
         '''
-        Once we kow the value of the key, It will be easy to dtore the elements at the key value.
-        Hence, we need to find an empty slot we start at the slot that corresponds to the hash value of the key.
-        If that slot is empty, we insert our item there.
+            Once we kow the value of the key, It will be easy to dtore the elements at the key value.
+            Hence, we need to find an empty slot we start at the slot that corresponds to the hash value of the key.
+            If that slot is empty, we insert our item there.
 
-        However, if the slot is not empty and the key of the item is not the same as our current key,
-        then we have a collision.
-        One way of resolving this kind of collision is to find slot from the position of the collision;
-        this collision resolution process is called open addressing. We can do this by linearly looking
-        for the next available slo by adding 1 to the preiou hash value where we get the collision.
-        This systematic way of visiting each slot is a linear way of resolving collisions and is called
-        Linear probing.
+            However, if the slot is not empty and the key of the item is not the same as our current key,
+            then we have a collision.
+            One way of resolving this kind of collision is to find slot from the position of the collision;
+            this collision resolution process is called open addressing. We can do this by linearly looking
+            for the next available slo by adding 1 to the preiou hash value where we get the collision.
+            This systematic way of visiting each slot is a linear way of resolving collisions and is called
+            Linear probing.
         '''
         while self.slots[h] is not None:
             if self.slots[h].key is key:
@@ -79,14 +78,13 @@ class HashTable:
         if self.slots[h] is None:
             self.count += 1
         self.slots[h] = item
-        
     '''
-    To retrieve values from the hash table , the value stored corresponding to the key would be returned.
-    Here, we will see the implementation of the retrieval method - get()
-    
-    First we compute the hash value of the key then we look in the table for the key at computed hash value
-    if key item matches the stored key value at that location, the corresponding value is retrieved.
-    If that doesn't matches, we add 1 to the sum of the ordinal values of all the charcaters 
+        To retrieve values from the hash table , the value stored corresponding to the key would be returned.
+        Here, we will see the implementation of the retrieval method - get()
+
+        First we compute the hash value of the key then we look in the table for the key at computed hash value
+        if key item matches the stored key value at that location, the corresponding value is retrieved.
+        If that doesn't matches, we add 1 to the sum of the ordinal values of all the charcater
     '''
     def get(self, key):
         h = self._hash(key) # compute hash for the given key
@@ -102,11 +100,10 @@ class HashTable:
     def __getitem__(self, key):
         return self.get(key)
 '''
-Using the put() and get() methods doesn't look very convenient to use. However, we would have preffered
-our hashtable as a list, as it would be easier to use.
+    Using the put() and get() methods doesn't look very convenient to use. However, we would have preffered
+    our hashtable as a list, as it would be easier to use.
 
-This can be easily done with special methods, __setitem__() and __getitem__().
-
+    This can be easily done with special methods, __setitem__() and __getitem__().
 '''
 
 ht = HashTable()
@@ -122,6 +119,6 @@ for key in("good","better","best","worst", "ad","ga"):
 print("The no. of total elements is : {}".format(ht.count))
 
 '''
-Here we hae only used linear proing to deal with collisons but this is not the best solution.
-There areother strategies like chaining that we must look into.
+    Here we hae only used linear proing to deal with collisons but this is not the best solution.
+    There areother strategies like chaining that we must look into.
 '''
