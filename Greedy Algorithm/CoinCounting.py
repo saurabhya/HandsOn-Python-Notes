783e1992c85626eec56213ef721ccda5763e87d9
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-02 21:19:22 +0530

    updating comments

--- Greedy Algorithm/CoinCounting.py
+++ Greedy Algorithm/CoinCounting.py
@@ -1,18 +1,18 @@
 '''
-COIN COUNTING PROBLEM
+    COIN COUNTING PROBLEM
 
-we wish to compute the minimum number of coin required to make a given amount A, where we have an infinte
-supply of the given coin's values.
+    we wish to compute the minimum number of coin required to make a given amount A, where we have an infinte
+    supply of the given coin's values.
 
-The algorithm to obtain the minimum numer of coins to provide a given amount A using denominations
-(a1,a2,a3...an):
-1. We sort the list of denominations (a1,a2,a3...an).
-2. we get the largest denominations in (a1,a2,a3...an) which is smaller than A.
-3. We obtain the division by dividing A by the largest denominations.
-4. We get remaining amount of A by getting remainder.
-5. If the value of A becomses 0, we return the result.
-6. Else if the value of A is greater than 0, we append the largest denomination
-   and division variable in the result variable. And repeat the steps 2-5.
+    The algorithm to obtain the minimum numer of coins to provide a given amount A using denominations
+    (a1,a2,a3...an):
+    1. We sort the list of denominations (a1,a2,a3...an).
+    2. we get the largest denominations in (a1,a2,a3...an) which is smaller than A.
+    3. We obtain the division by dividing A by the largest denominations.
+    4. We get remaining amount of A by getting remainder.
+    5. If the value of A becomses 0, we return the result.
+    6. Else if the value of A is greater than 0, we append the largest denomination
+    and division variable in the result variable. And repeat the steps 2-5.
 '''
 def basic_small_change(denom, total_amount):
     sorted_denom = sorted(denom, reverse= True)
@@ -26,10 +26,10 @@ def basic_small_change(denom, total_amount):
     return number_of_denoms
 
 '''
-However, there are some possible instances where this algorithm may fail.
+    However, there are some possible instances where this algorithm may fail.
 
-A better greedy algorithm is presetedhere. this time, the funtion retuens
-a list of tuples that allow us to invistigate the best results.
+    A better greedy algorithm is preseted here. This time, the funtion returns
+    a list of tuples that allow us to invistigate the best results.
 '''
 
 def optimal_small_change(denom, total_amount):
--- Hashing/Hash.py
+++ Hashing/Hash.py
@@ -1,9 +1,9 @@
 '''
-Hashing is aconcept in which, when we give data of an arbitrary size to a function, 
-we get a small simplified value. This function is called a hash function. hashing uses 
-a hash function that maps the given data to another range of data, so that a new range
-of data can be used as an index in the hash table. More specifically, we will use 
-hashing to convert strings into intergers. 
+    Hashing is aconcept in which, when we give data of an arbitrary size to a function,
+    we get a small simplified value. This function is called a hash function. hashing uses
+    a hash function that maps the given data to another range of data, so that a new range
+    of data can be used as an index in the hash table. More specifically, we will use
+    hashing to convert strings into intergers.
 '''
 def tryHash(s):
     hv = 0
@@ -18,18 +18,17 @@ for item in ('hello world', 'world hello', 'gello xorld'):
 print("\n")
 
 '''
-A perfect hashing function is the one by which we get a unique hash value for a given
-string(it can be any data type). In practice, most of the hashing functions are imperfect
-and face collisions. This ,eans that a hash function gives the same hash value to more than
-one string; that is undesirable because a perfect hash function should return a unique hash 
-value to a string. Normally, hashing functions need to be very fast, so trying to create a 
-function that gives us a unique hash value for each string is normally nott possible.
-Hence, we accept this fact and we know that we may get some collisions, that is , two
-or more strings may have the same hash value. therefore, we try to find a strategy to 
-resolve the collisions rather trying to find a perfect hashing function.  
+    A perfect hashing function is the one by which we get a unique hash value for a given
+    string(it can be any data type). In practice, most of the hashing functions are imperfect
+    and face collisions. This ,eans that a hash function gives the same hash value to more than
+    one string; that is undesirable because a perfect hash function should return a unique hash
+    value to a string. Normally, hashing functions need to be very fast, so trying to create a
+    function that gives us a unique hash value for each string is normally nott possible.
+    Hence, we accept this fact and we know that we may get some collisions, that is , two
+    or more strings may have the same hash value. therefore, we try to find a strategy to
+    resolve the collisions rather trying to find a perfect hashing function.
 
-For example:
-
+    For example:
 '''
 
 def myHash(s):
@@ -44,13 +43,13 @@ for item in ('hello world', 'world hello', 'gello xorld'):
     print("{}: {}".format(item, myHash(item)))
 
 '''
-We can see that, this time, we get different hash values for these three strings.
-Still, this is not a perfect hash. 
-Let's try the strings, ad and ga
+    We can see that, this time, we get different hash values for these three strings.
+    Still, this is not a perfect hash.
+    Let's try the strings, ad and ga
 '''
 for item in ('ad', 'ga'):
     print("{}: {}".format(item, myHash(item)))
 '''
-We still get the same hash values for two differernt strings. therefore, we need to 
-devise a strategy for resolving such collisions.
+    We still get the same hash values for two differernt strings. therefore, we need to
+    devise a strategy for resolving such collisions.
 '''
