8704c181cd1f7e59118e9b9753014bd234be8b31
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-07 22:12:33 +0530

    Formatiing comments

--- Stacks and queues/StackBasedQueue.py
+++ Stacks and queues/StackBasedQueue.py
@@ -1,11 +1,10 @@
 '''
-Queues can also be implemented using two stacks, We initially 
-set two instance variable to create an empty queue upon initialization.
-These are the stacks that will help us to implement a queue. 
-The stack, in this case, are simply Python lists that allow us to 
-call push and pop methods on them, which eventually sllow us
-to get the functionality of enqueue and dequeue operations.
-
+    Queues can also be implemented using two stacks, We initially 
+    set two instance variable to create an empty queue upon initialization.
+    These are the stacks that will help us to implement a queue. 
+    The stack, in this case, are simply Python lists that allow us to 
+    call push and pop methods on them, which eventually sllow us
+    to get the functionality of enqueue and dequeue operations.
 '''
 
 class Queue:
@@ -13,25 +12,25 @@ class Queue:
         self.inbound_stack = []
         self.outbound_stack = []
         '''
-        The inbound_stack is only used to store elements that are
-        added to the queue. No other operation can be performed 
-        on this stack.
+            The inbound_stack is only used to store elements that are
+            added to the queue. No other operation can be performed 
+            on this stack.
         '''
     def enqueue(self, data):
         '''
-        data is passed to the append method of the inbound_stack in
-        the queue class. Further, the append method is used to mimic
-        the push operation, which pushes items to the top of the stack
+            data is passed to the append method of the inbound_stack in
+            the queue class. Further, the append method is used to mimic
+            the push operation, which pushes items to the top of the stack
         
         '''
         self.inbound_stack.append(data)
 
     def dequeue(self):
         '''
-        The dequeue operation is used to delete the elements from the
-        queue in the order of items added. New elemets added to our 
-        queue end up in the inbound_stack. We shall delete the elements
-        from our queue only through the outbound_stack.
+            The dequeue operation is used to delete the elements from the
+            queue in the order of items added. New elemets added to our 
+            queue end up in the inbound_stack. We shall delete the elements
+            from our queue only through the outbound_stack.
         '''
         if not self.outbound_stack:
             while self.inbound_stack:
