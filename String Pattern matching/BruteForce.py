5e0584d407674623198d8131c9bed4c28f6e0893
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-09 23:16:15 +0530

    updating comments

--- String Pattern matching/BruteForce.py
+++ String Pattern matching/BruteForce.py
@@ -1,12 +1,12 @@
 '''
-The brute-force or naive approach for the pattern matching algorithm, is very basic. Using this, we simply test all the possible
-ombinations of the input pattern in the given string to find the position of the ocurence of the pattern. this algorithm is very
-naive and is not suitable if the text is very long.
+    The brute-force or naive approach for the pattern matching algorithm, is very basic. Using this, we simply test all the possible
+    ombinations of the input pattern in the given string to find the position of the ocurence of the pattern. this algorithm is very
+    naive and is not suitable if the text is very long.
 
-here, we start by comparing the characters of the pattern and the text string one by one, and if all the characters of the
-pattern are matched with text, we return the index position of the text where the first character of the pattern is placed.
-If any character of the pattern is mismatched with the text string, we shift the pattern by one place. we continue comparing
-the pattern and text string by shifting the pattern by one index position.
+    Here, we start by comparing the characters of the pattern and the text string one by one, and if all the characters of the
+    pattern are matched with text, we return the index position of the text where the first character of the pattern is placed.
+    If any character of the pattern is mismatched with the text string, we shift the pattern by one place. we continue comparing
+    The pattern and text string by shifting the pattern by one index position.
 '''
 
 def brute_force(text, pattern):
