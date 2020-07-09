e4c3a65e5ac1822388f10342142bdbd524156f11
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-09 23:19:43 +0530

    formatting content

--- String Pattern matching/BuiltIn.py
+++ String Pattern matching/BuiltIn.py
@@ -1,8 +1,8 @@
 '''
-The prefix of a string s is present at the starting of the string. For example 'He' is the prefix of string "Hello World"
-The suffix of a string s is present at the end of the string. For example 'd' id the suffix of string "Hello World"
+    The prefix of a string s is present at the starting of the string. For example 'He' is the prefix of string "Hello World"
+    The suffix of a string s is present at the end of the string. For example 'd' id the suffix of string "Hello World"
 
-Python has buil-in functions to check whether a string has a given prefix or suffix:
+    Python has buil-in functions to check whether a string has a given prefix or suffix:
 '''
 string = "Lorem ipsum dolor sit amet consectetur adioiscing elit"
 suffix = "elit"
