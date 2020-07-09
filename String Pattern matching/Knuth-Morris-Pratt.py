d62503fdee41b900d9d934af88f90f4a923685c5
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-09 23:21:12 +0530

    formatting comments

--- String Pattern matching/Knuth-Morris-Pratt.py
+++ String Pattern matching/Knuth-Morris-Pratt.py
@@ -1,30 +1,30 @@
 '''
-The Knuth-Morris-Pratt(KMP) algorithm is a pattern matching algorithm that is based on a precomputed prefix function
-that stores the information of an overlapping text portion in the pattern. The KMP algorithm preprocesses this pattern
-to avoid this unnecessary comparisons when using the prefix function. the algorithm utilizes the prefix function to estimate
-how much the pattern should be shifted to search the apttern in the text string whenever we get a mismatch.
-The KMP algorithm is efficient as it minimizes the comparisons of the given patterns with the respect of the text string.
+    The Knuth-Morris-Pratt(KMP) algorithm is a pattern matching algorithm that is based on a precomputed prefix function
+    that stores the information of an overlapping text portion in the pattern. The KMP algorithm preprocesses this pattern
+    to avoid this unnecessary comparisons when using the prefix function. the algorithm utilizes the prefix function to estimate
+    how much the pattern should be shifted to search the apttern in the text string whenever we get a mismatch.
+    The KMP algorithm is efficient as it minimizes the comparisons of the given patterns with the respect of the text string.
 
-The Prfix Function
-The prefix function(also known as failure function) finds the pattern in the pattern itself. It rises to find how much the previous
-comparisons can be reused due to repetition in the pattern itself when there is a mismatch. It has a value that is
-mainly the longest prefix, which is also a suffix.
+    The Prfix Function
+    The prefix function(also known as failure function) finds the pattern in the pattern itself. It rises to find how much the previous
+    comparisons can be reused due to repetition in the pattern itself when there is a mismatch. It has a value that is
+    mainly the longest prefix, which is also a suffix.
 
-The KMP pattern amtching algorithm uses a pattern that has overlap in the pattern itself so that it avoids unncessary
-comparisons. The main idea behind KMP algorithm is to dteect how much the pattern should be shifted, based on the overlaps
-in the patterns. The algrithm works as follows:
-1. First, we precompute the prefix function for the given pattern and initialize a counter, q, that represents the number
-   of charaters that matched.
-2. We startby comparing the first character of the pattern with the first character of the text string, and we compare the
-   next character.
-3. If there is a mismatch, then we assign the value of the precomputed prefix function for q to the index value of q.
-4. We continue searching for the pattern in the text string until we reach the end of the text, that is, if we do not
-   find any matches. If all of the characters in the pattern are matched in the text sttring, we return the position
-   where the pattern is matched in the text and continue to search for another match.
+    The KMP pattern amtching algorithm uses a pattern that has overlap in the pattern itself so that it avoids unncessary
+    comparisons. The main idea behind KMP algorithm is to dteect how much the pattern should be shifted, based on the overlaps
+    in the patterns. The algrithm works as follows:
+    1. First, we precompute the prefix function for the given pattern and initialize a counter, q, that represents the number
+       of charaters that matched.
+    2. We startby comparing the first character of the pattern with the first character of the text string, and we compare the
+       next character.
+    3. If there is a mismatch, then we assign the value of the precomputed prefix function for q to the index value of q.
+    4. We continue searching for the pattern in the text string until we reach the end of the text, that is, if we do not
+       find any matches. If all of the characters in the pattern are matched in the text sttring, we return the position
+       where the pattern is matched in the text and continue to search for another match.
 
-The KMP algorithm has two phases, the preprocessing phase, which is where we compute the prefix function, it takes
-the space and time complexity of O(m) and further, in the second phase, that searching, the KMP algorithm takes time
-complexity of  O(n).
+    The KMP algorithm has two phases, the preprocessing phase, which is where we compute the prefix function, it takes
+    the space and time complexity of O(m) and further, in the second phase, that searching, the KMP algorithm takes time
+    complexity of  O(n).
 '''
 
 def pfun(pattern): # function to generate prefix function for the given pattern
