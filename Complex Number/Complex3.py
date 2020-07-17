10b0068a8ba2179d053c805572375148c7dd3d8f
saurabh919yadav <saurabh.yadav919@gmail.com>
2020-07-17 22:09:50 +0530

    Create Complex3.py

---
+++ Complex Number/Complex3.py
@@ -0,0 +1,12 @@
+'''
+    Python cmath module
+    It provides access to mathematical functions for complex numbers.
+    Let's look at some of the important features of complex numbers using math module function.
+
+    Phase of complex number
+    The phase of a complex number is the angle between the real axis and
+    the vector representing the imaginary part.
+    The phase returned by math and cmath modules are in radians an dwe use the numpy.degrees() function to convert it to degrees.
+'''
+
+
